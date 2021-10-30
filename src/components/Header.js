2455e00dbb9c7b9d5dc079a52313c629ca883013
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-30 16:04:15 +0530

    3 files modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -109,6 +109,111 @@ export default function Header() {
           <Link to="/chat">Chat</Link>
         </li>
       </ul> */}
+      <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center">
+          <h1 class="logo me-auto">
+            <a href="index.html">Mentor</a>
+          </h1>
+
+          <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+              <li>
+                <a class="active" href="index.html">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="about.html">About</a>
+              </li>
+              <li>
+                <a href="courses.html">Courses</a>
+              </li>
+              <li>
+                <a href="trainers.html">Trainers</a>
+              </li>
+              <li>
+                <a href="events.html">Events</a>
+              </li>
+              <li>
+                <a href="pricing.html">Pricing</a>
+              </li>
+
+              <li class="dropdown">
+                <a href="#">
+                  <span>Drop Down</span> <i class="bi bi-chevron-down"></i>
+                </a>
+                <ul>
+                  <li>
+                    <a href="#">Drop Down 1</a>
+                  </li>
+                  <li class="dropdown">
+                    <a href="#">
+                      <span>Deep Drop Down</span>{' '}
+                      <i class="bi bi-chevron-right"></i>
+                    </a>
+                    <ul>
+                      <li>
+                        <a href="#">Deep Drop Down 1</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 2</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 3</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 4</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 5</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#">Drop Down 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Drop Down 3</a>
+                  </li>
+                  <li>
+                    <a href="#">Drop Down 4</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="contact.html">Contact</a>
+              </li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav>
+
+          <a href="courses.html" class="get-started-btn">
+            Get Started
+          </a>
+        </div>
+        <section
+          id="hero"
+          class="d-flex justify-content-center align-items-center"
+        >
+          <div
+            class="container position-relative"
+            data-aos="zoom-in"
+            data-aos-delay="100"
+          >
+            <h1>
+              Learning Today,
+              <br />
+              Leading Tomorrow
+            </h1>
+            <h2>
+              We are team of talented designers making websites with Bootstrap
+            </h2>
+            <a href="courses.html" class="btn-get-started">
+              Get Started
+            </a>
+          </div>
+        </section>
+      </header>
     </div>
   );
 }
